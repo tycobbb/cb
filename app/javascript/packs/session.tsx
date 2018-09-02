a438414812de1efc9b610689d8ad9689c497542e
Ty Cobb <ty.cobb.m@gmail.com>
2018-09-01 19:45:02 -0500

    fix js loading, render session pack

--- app/javascript/packs/session.tsx
+++ app/javascript/packs/session.tsx
@@ -1,5 +1,5 @@
 import { h, render } from "preact"
 import { Session } from "../app/session"
 
-const root = document.getElementById("root")
+const root = document.getElementById("session-root")
 root && render(<Session />, root)
